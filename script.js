d21a7b318b1eae7a27518a33973afe955340149c
Mi13285 <mixkud95@inbox.ru>
2024-05-18 06:21:29 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let element = document.body.firstElementChild;
+console.log(element);
+let li = document.querySelectorAll("li")[0];
+console.log(li);
+let p = li.innerHTML;
+console.log(p);
+let liMany = document.querySelectorAll("li");
+console.log(liMany);
+for (many of liMany) {
+  console.log(many.innerHTML);
+}
+
+let article = document.getElementById("12");
+console.log(article);
+let articleOne = article.innerHTML;
+console.log(articleOne);
+let x = document.querySelector("h1");
+console.log(x);
+let xOne = x.innerHTML;
+console.log(xOne);
+let y = document.querySelector("p");
+let POne = y.innerHTML;
+console.log(POne);
